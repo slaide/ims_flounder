eca5312f6d182f7f0ec7f8fb1db6468707c19cc4
slaide <patrick.hennig@fau.de>
2021-01-28 16:10:16 +0100

    server can now distinguish between accessible and non-accessible paths, and responds with special page when not accessible

--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
 const http=require("http")
 const fs=require("fs")
 
+function read_404_page(){
+    return fs.readFileSync("./html/404.html",encoding.utf8)
+}
+function read_homepage(){
+    return fs.readFileSync("./html/index.html",encoding.utf8)
+}
+
+const content={
+    html:{"Content-Type":"text/html"},
+}
+const encoding={
+    utf8:{encoding:"utf8"},
+}
+const request_handler={
+    "/":function(req,res){
+        res.writeHeader(200,content.html)
+        const file=read_homepage()
+        res.write(file)
+        res.end()
+    }
+}
+
 const server=http.createServer((req,res)=>{
-    res.writeHeader(200,{"Content-Type":"text/html"})
-    const file=fs.readFileSync("./html/index.html",{encoding:"utf8",flag:"r"})
-    res.write(file)
-    res.end()
+    //console.log(req.url,req.method)
+    handler=request_handler[req.url]
+    if(handler){
+        handler(req,res)
+    }else{
+        res.writeHeader(404,content.html)
+        res.end(read_404_page())
+    }
 })
 server.listen(8080,"127.0.0.1")
