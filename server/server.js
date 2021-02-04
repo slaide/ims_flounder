f4e37eebf628153ca6b999f7d84aaa9d3afc076c
slaide <patrick.hennig@fau.de>
2021-02-04 09:39:02 +0100

    extended functionality to connect to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http=require("http")
 const fs=require("fs")
 const mysql=require("mysql")
+const url = require("url")
 
 function read_404_page(){
     return fs.readFileSync("../html/404.html",encoding.utf8)
@@ -26,8 +27,9 @@ function send_static_data(req,res){
     const html_folder=fs.readdirSync("../html");
     if(html_folder.includes(req.url.substr(1))){
         res.writeHeader(200,content.from_filename(req.url))
-        res.write(fs.readFileSync("../html"+req.url))
+        res.write(fs.readFileSync("../html"+req.url,encoding.utf8))
         res.end()
+        console.log(req.url, "was sent.")
         return true
     }
 
@@ -57,17 +59,17 @@ const encoding={
 }
 const request_handler={
     "/":function(req,res){
-        res.writeHeader(200,content.html)
-        const file=read_homepage()
-        res.write(file)
-        res.end()
+        req.url="/index.html"
+        if(!send_static_data(req,res)){
+            throw("could not find homepage file? aborting.")
+        }
     },
     "/login":function(req,res){
         let connection_data={
             host:"localhost",
             user:"root",
             password:"",
-            database:"doesnotexist"
+            database:"lims"
         }
 
         var connection=mysql.createConnection(connection_data)
@@ -82,17 +84,23 @@ const request_handler={
         }
     
         connection.connect(
-            function(e){
-                if(e){
-                    if(e.code=="ECONNREFUSED"){
+            function(err){
+                if(err){
+                    if(err.code=="ECONNREFUSED"){
                         console.log("database is not running")
-                    }else if(e.sqlMessage){
-                        console.log("database error: ",e.sqlMessage)
+                    }else if(err.sqlMessage){
+                        if(err.code=="ER_BAD_DB_ERROR"){
+                            console.log("database not found. creating...")
+                            var create_database_connection=mysql.createConnection({host:"localhost",user:"root",password:""})
+                            create_database_connection.connection((err)=>{if(err){console.log("database creation failed.",err)}})
+                            return
+                        }
+                        console.log("database error: ",err.sqlMessage)
                     }else{
-                        console.log(e)
+                        console.log("unexpected sql error: ",err)
                     }
 
-                    if(!e.fatal){
+                    if(!err.fatal){
                         end_connection(connection)
                     }
                     abort_login()
@@ -101,11 +109,16 @@ const request_handler={
 
                 console.log("connected successfully")
                 
+                var login_data={username:null,password:null}
                 connection.query("select * from users where username=? and password=?;",[login_data.username,login_data.password],function(err,results,fields){
                     if(err){
-                        console.log(e)
+                        if(err.code=="ER_NO_SUCH_TABLE"){
+                            console.log("table 'users' does not exist.")
+                        }else{
+                            console.log("error on login data check: ",err)
+                        }
 
-                        if(!e.fatal){
+                        if(!err.fatal){
                             end_connection(connection)
                         }
                         abort_login()
@@ -133,23 +146,71 @@ const request_handler={
     }
 }
 
+//parse data from get/post methods
+//'then' is callback executed after data has been parsed
+function parse_data(req,then){
+    if(req.method=="POST"){
+        var chunks=""
+        req.on("data",(chunk)=>{
+            chunks+=chunk
+        })
+        req.on("end",()=>{
+            var params=new url.URLSearchParams(chunks)
+            var ret={method:"POST"}
+            params.forEach((value,key,searchparams)=>{
+                ret[key]=value
+            })
+            then(ret)
+        })
+    }else{
+        var params=new url.URLSearchParams(req.url)
+        var ret={method:"GET"}
+        params.forEach((value,key,searchparams)=>{
+            ret[key]=value
+        })
+        then(ret)
+    }
+}
+
 const server=http.createServer((req,res)=>{
     try{
-        //console.log(req.url,req.method)
+        /*
+        //for explanation on client-server interaction
+        if(!send_static_data(req,res)){
+            parse_data(req,(parsed)=>{
+                console.log(req.url,parsed)
+                var file=fs.readFileSync("../html/index.html",encoding.utf8)
+                file=file.replace("$$NAME$$",parsed.name)
+                res.writeHeader(200,content.html)
+                res.write(file)
+                res.end()
+            })
+        }
+
+        return
+        */
+
+        //check if url is known to server
         handler=request_handler[req.url]
         if(handler){
             handler(req,res)
             return
+        //if url is not a special case
         }else{
+            //check if url is a static file, send the file if so
             if(!send_static_data(req,res)){
+                //otherwise send 404
+                console.log(req.url, " could not be found.")
                 res.writeHeader(404,content.html)
                 res.end(read_404_page())
             }
             return
         }
     }catch(e){
+        //force_shutdown is set on graceful shutdown request
         if(!e.force_shutdown){
             console.log(e)
+        //every other throw is fatal
         }else{
             console.log("closing web server.")
             server.close()
