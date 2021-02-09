38ff738ff6c8f11c98953a7f6326522d32dc7957
slaide <patrick.hennig@fau.de>
2021-02-09 14:51:53 +0100

    fixed small error with callback hell

--- server/login.js
+++ server/login.js
@@ -8,39 +8,42 @@ function check_login_data(req,res){
     utility.parse_data(req,(user_data)=>{
         console.log(user_data)   //user_data.password, user_data.email is a string 
         
-        var connection=database.connect_database()
+        database.connect_database((connection)=>{
         
-        //result obj is array, containing each row as object
-        //var db_email=connection.query("SELECT Email FROM user WHERE Email = 'user_data.email'")
-        //console.log(db_email)
-        connection.query("SELECT Email FROM user", function (err, db_email) {
-            if(err) throw err; 
-            console.log(db_email)
-        })
-        
-        //check email match 
-        if(db_email.lenght==1){
-            //check password match 
-            console.log("Email match found")
-            var db_password=connection.query("SELECT Password FROM user WHERE Email = 'user_data.email'")
-            if(user_data.password.localeCompare(db_password)==0){
-                console.log("Password match")
-                //Login sucefully 
-                //send back file overview.html
+            //result obj is array, containing each row as object
+            //var db_email=connection.query("SELECT Email FROM user WHERE Email = 'user_data.email'")
+            //console.log(db_email)
+            connection.query("SELECT Email FROM user", function (err, db_email) {
+                if(err) throw err; 
+                console.log(db_email)
+            })
+            
+            //note from patrick: all of this below goes into the function block above. good luck
+
+            //check email match 
+            if(db_email.lenght==1){
+                //check password match 
+                console.log("Email match found")
+                var db_password=connection.query("SELECT Password FROM user WHERE Email = 'user_data.email'")
+                if(user_data.password.localeCompare(db_password)==0){
+                    console.log("Password match")
+                    //Login sucefully 
+                    //send back file overview.html
+                    
+                }
+                else{
+                    console.log("No password match")
+                    //Password don't match. What send back? 
+                }
                 
             }
+            //no email match 
             else{
-                console.log("No password match")
-                //Password don't match. What send back? 
+            console.log("No email match")
+            //What send back here?   
             }
-            
-        }
-        //no email match 
-        else{
-           console.log("No email match")
-           //What send back here?   
-        }
-        //database disconnnect
+            //database disconnnect
+        })
         
     })
 }
