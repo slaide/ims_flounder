ed9823d025fe1d79c45051a02350121837e22e65
slaide <patrick.hennig@fau.de>
2021-02-10 12:26:42 +0100

    initial commit for room/machine user interaction

---
+++ server/machines.js
@@ -0,0 +1,9 @@
+function get_schedule(req,res){
+    throw "unimplemented"
+}
+module.exports.get_schedule=get_schedule
+
+function set_schedule_slot(req,res){
+    throw "unimplemented"
+}
+module.exports.set_schedule_slot=set_schedule_slot
