492ae6feb8083fcb5697d215198b44343551387a
slaide <patrick.hennig@fau.de>
2021-02-22 13:39:13 +0100

    added error messaging code to reserve_timeslot function

--- server/reserve_instrument.js
+++ server/reserve_instrument.js
@@ -14,50 +14,86 @@ function reserve_instrument(req,res){
     database.connect_database((connection,error)=>{
         if(error){
             console.log("failed to connect to db for timeslot reservation")
+            
             if(!error.fatal){
                 database.disconnect(connection)
             }
+
+            res.writeHeader(200,utility.content.json)
+            res.end(JSON.stringify({error:"failed to connect to db for timeslot reservation"}))
+
             return
         }
         utility.parse_data(req,(data)=>{
-            if(!data.InsID || !data.StartTime || !data.EndTime || !data.Username || !data.SSN){
-                console.log("request is missing at least one these attributes: InsID, StartTime, EndTime, Username, SSN")
-                database.disconnect(connection)
-                return
+            for(attribute of "StartTime EndTime InsId Username SSN".split(" ")){
+                if(!data[attribute]){
+                    const error_message="request is missing the attribute '"+attribute+"'"
+                    console.log(error_message)
+
+                    res.writeHeader(200,utility.content.json)
+                    res.end(JSON.stringify({error:error_message}))
+
+                    database.disconnect(connection)
+                    return
+                }
             }
-            connection.query("select * from booking where ? and (StartTime>=? or EndTime<=?);",[data.StartTime,data.EndTime],(error,results,fields)=>{
+            connection.query("select * from booking where Ins_ID=? and (Start_time>=? or End_time<=?);",[data.InsId,data.StartTime,data.EndTime],(error,results,fields)=>{
                 if(error){
-                    console.log("error checking for free timeslot",error)
+                    console.log("error checking for free timeslot ",error)
+
                     if(!error.fatal){
                         database.disconnect(connection)
                     }
+
+                    res.writeHeader(200,utility.content.json)
+                    res.end(JSON.stringify({error:"error selecting potentially overlapping reservations"}))
+
                     return
                 }
                 if(results.length!=0){
                     console.log("timeslot already reserved: ",data)
+
                     res.writeHeader(200,utility.content.json)
-                    res.end("{'error':'timeslot already reserved'}")
+                    res.end(JSON.stringify({'error':'timeslot already reserved'}))
+
                     database.disconnect(connection)
                     return
                 }
-                connection.query("insert into booking(Note, start_Time, End_Time, `Status`, SSN, Inst_ID)) values('',?,?,'booked',?,?)",[data.StartTime,data.EndTime,data.SSN,data.InsID],(error,results,fields)=>{
+                connection.query("insert into booking(Note, Start_time, End_time, Status, SSN, Inst_ID)) values('',?,?,'booked',?,?)",[data.StartTime,data.EndTime,data.SSN,data.InsID],(error,results,fields)=>{
                     if(error){
                         console.log("error reserving free timeslot",error)
+
                         if(!error.fatal){
                             database.disconnect(connection)
                         }
+
+                        res.writeHeader(200,utility.content.json)
+                        res.end(JSON.stringify({error:"failed to reserve timeslot due to internal error"}))
+
                         return
                     }
                     if(results.length!=0){
-                        console.log("timeslot not reserved for some reason")
+                        console.log("timeslot could not be reserved")
+
                         res.writeHeader(200,utility.content.json)
-                        res.end("{'error':'timeslot could not be reserved'}")
+                        res.end(JSON.stringify({'error':'timeslot could not be reserved'}))
+
                         database.disconnect(connection)
                         return
                     }
+
+                    res.writeHeader(200,utility.content.json)
+                    res.end(JSON.stringify({'result':'timeslot successfully reserved'}))
+
+                    database.disconnect(connection)
+                    return
                 })
             })
         })
     })
 }
 module.exports.reserve_instrument=reserve_instrument
+
+how link child account works?
+contradicting vaccines?
+on time?
