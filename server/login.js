67a7cf20de561733ed4adb34d86525948facb5ef
unknown <oliviamnorlin@gmail.com>
2021-02-22 16:14:37 +0100

    replacing variables in overview.html

--- server/login.js
+++ server/login.js
@@ -12,44 +12,33 @@ function check_login_data(req,res){
         
         database.connect_database((connection)=>{
         
-            connection.query("SELECT Email, Password FROM user WHERE Email = ?", [user_data.email], (err, result)=> {
+            connection.query("SELECT Email, Password, SSN FROM user WHERE Email = ?", [user_data.email], (err, result)=> {
                 if(err) throw err;
 
                 db_data=JSON.parse(JSON.stringify(result))  
                 console.log('>> db_data: ', db_data) 
                 
-                //Check email match 
-                if(db_data.length!=0){
+                //Check email and password match 
+                if(db_data.length!=0 && user_data.password.localeCompare(db_data[0].Password)==0){
                     console.log('>> db_data email: ', db_data[0].Email);
                     //console.log('>> typeof(db_data[0].Email): ', typeof(db_data[0].Email));  //db_data[0].Email is a string 
-                    console.log(">> Email match found")
+                    console.log(">> Email and password match found, login succesfull")
 
-                    //check password match
-                    if(user_data.password.localeCompare(db_data[0].Password)==0){
-                        console.log(">> Password match")
-                        console.log('>> Login succesfull')
-
-                        res.end(fs.readFileSync("../html/overview.html",utility.encoding.utf8))
-                        console.log('>> overview.html file sent back to client')
-                        database.disconnect(connection)
-                        console.log('>> Database disconnected')
-                    }
-                    else{
-                        console.log(">> No password match")
-                        res.end(fs.readFileSync("../html/index.html",utility.encoding.utf8).replace("$$LOGINSUCCESS$$", "true"))
-                        console.log('>> index.html file sent back to client with LOGINSUCCESS=true')
-                        database.disconnect(connection)
-                        console.log('>> Database disconnected')
-                    }
+                    overview_string=fs.readFileSync("../html/overview.html",utility.encoding.utf8)
+                    overview_string.replace("$$EMAIL$$", db_data[0].Email) 
+                    overview_string.replace("$$SSN$$", db_data[0].SSN)
+                    res.end(overview_string)
+                    //res.end(fs.readFileSync("../html/overview.html",utility.encoding.utf8))
+                    console.log('>> overview.html file sent back to client')
+                    database.disconnect(connection)
+                    console.log('>> Database disconnected')
                 }
- 
                 else{
-                    console.log(">> No email match found")
+                    console.log(">> No match found, login unsuccesfull")
                     res.end(fs.readFileSync("../html/index.html",utility.encoding.utf8).replace("$$LOGINSUCCESS$$", "true"))
                     console.log('>> index.html file sent back to client with LOGINSUCCESS=true')
                     database.disconnect(connection)
                     console.log('>> Database disconnected') 
-                
                 }
             })
         }) 
