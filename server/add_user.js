30265d57b1cc56b6a2d3c600016540d5bf0193e7
slaide <patrick.hennig@fau.de>
2021-02-24 10:35:15 +0100

    added initial implementation of function add new users

---
+++ server/add_user.js
@@ -0,0 +1,61 @@
+const utility=require("./utility.js")
+const database=require("./database.js")
+
+/**
+ * Add a new user to the system
+ * currently expects client data: ssn, first_name, last_name, password, admin, phone_number, email, special_rights, immunocompromised
+ * currently responds with: error/success
+ * @param {Request} req Request object with client data
+ * @param {Response} res Reponse object
+ */
+function add_user(req,res){
+    database.connect_database((connection,error)=>{
+        if(error){
+            const error_message="failed to connect to db to add user"
+            console.log(error_message)
+            if(!error.fatal){
+                database.disconnect(connection)
+            }
+            res.writeHeader(200,utility.content.json)
+            res.end(JSON.stringify({error:error_message}))
+            return
+        }
+        utility.parse_data(req,(data)=>{
+            var add_user_data=[]
+            //make sure all of the expected data is here and defined
+            for(attribute of "ssn first_name last_name password admin phone_number email special_rights immunocompromised".split(" ")){
+                if(!data[attribute]){
+                    const error_message="request is missing the attribute '"+attribute+"'"
+                    console.log(error_message)
+
+                    res.writeHeader(200,utility.content.json)
+                    res.end(JSON.stringify({error:error_message}))
+
+                    database.disconnect(connection)
+                    return
+                }
+                //add data to array in sequence also used in db
+                add_user_data.push(data[attribute])
+            }
+
+            //make sure a user only revokes their own bookings?
+            connection.query("insert into user values ();",add_user_data,(error,results,fields)=>{
+                if(error){
+                    const error_message="failed to add user because "+error
+                    console.log(error_message)
+                    if(!error.fatal){
+                        database.disconnect(connection)
+                    }
+                    res.writeHeader(200,utility.content.json)
+                    res.end(JSON.stringify({error:error_message}))
+                    return
+                }
+
+                database.disconnect(connection)
+                res.writeHeader(200,utility.content.json)
+                res.end(JSON.stringify({result:"successfully added user"}))
+            })
+        })
+    })
+}
+module.exports.add_user=add_user;
