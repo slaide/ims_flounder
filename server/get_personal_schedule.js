51dc6e19c9e32e5d586f824be979e2b6b57a2bda
unknown <oliviamnorlin@gmail.com>
2021-02-26 13:54:03 +0100

    added bookingID

--- server/get_personal_schedule.js
+++ server/get_personal_schedule.js
@@ -3,6 +3,7 @@ const database=require("./database.js")
 
 //send personal schedule:
 //combine all reservations for a single user into one schedule
+//For Linnéa my_scheduel.html
 function get_personal_schedule(req,res){
     
     database.connect_database((connection,error)=>{
@@ -14,11 +15,12 @@ function get_personal_schedule(req,res){
             return
         }
         utility.parse_data(req,(data)=>{
+            console.log("TO ME")
             console.log('>>data: ', data)
             console.log('>> SSN: ',data.SSN) 
 
-            var sql="SELECT booking.Start_Time, booking.End_Time, instrument.Description FROM booking JOIN instrument ON booking.Ins_ID=instrument.Ins_ID WHERE booking.SSN = ?" 
-            
+            var sql="SELECT booking.Booking_ID, booking.Start_Time, booking.End_Time, instrument.Description FROM booking JOIN instrument ON booking.Ins_ID=instrument.Ins_ID WHERE booking.SSN = ?" 
+
             connection.query(sql, [data.SSN], (error, result)=> {
                 if(error){
                     console.log("error selecting attributes",error)
@@ -27,12 +29,13 @@ function get_personal_schedule(req,res){
                     }
                     return
                 }
+                console.log(">>result:", result)
                 db_booking=JSON.parse(JSON.stringify(result))  
                 console.log('>> db_booking: ', db_booking) 
 
                 var ret=[];
                 for(item of db_booking){
-                    ret.push({StartTime:item.Start_Time, EndTime:item.End_Time, Description:item.Description})
+                    ret.push({BookingID:item.Booking_ID, StartTime:item.Start_Time, EndTime:item.End_Time, Description:item.Description})
                 }
                 
                 console.log('>>ret:', ret) 
