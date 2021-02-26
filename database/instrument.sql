98233951ee1ba7db7e1cfb8e746ff5047e3aab1a
SelmaBozorg <78197068+SelmaBozorg@users.noreply.github.com>
2021-02-26 10:45:46 +0100

    Add files via upload

--- database/instrument.sql
+++ database/instrument.sql
@@ -7,7 +7,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 
 CREATE TABLE instrument (
-  Ins_ID varchar(40) NOT NULL,
+  Ins_ID int NOT NULL,
   Description varchar(40) NOT NULL DEFAULT 'NOT NULL',
   Serial int NOT NULL,
   Proc_date date NOT NULL,
@@ -15,20 +15,24 @@ CREATE TABLE instrument (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO instrument (Ins_ID, Description, `Serial`, Proc_date, Room_ID) VALUES
-('ins1', 'Mass spec ', 6047, '2020-12-14', 'r2'),
-('ins2', 'Laser ', 5282, '2021-01-02', 'r3'),
-('ins4', 'Pipette 3000', 3940, '2020-12-30', 'r1'),
-('ins3', 'Pipette', 2016, '2021-01-24', 'r3'),
-('ins5', 'Mass spec 2000', 1244, '2021-01-24', 'r1'),
-('wb1', 'Workbech ', 5656, '2021-02-03', 'r2'),
-('wb2', 'Workbech ', 4096, '2020-12-01', 'r3'),
-('wb3', 'Workbech ', 2687, '2021-02-01', 'r1'),
-('wb4', 'Workbech ', 3487, '2020-12-14', 'r1');
+(1, 'Mass spec ', 6047, '2020-12-14', '2'),
+(2, 'Laser ', 5282, '2021-01-02', '3'),
+(3, 'Pipette 3000', 3940, '2020-12-30', '1'),
+(4, 'Pipette', 2016, '2021-01-24', '3'),
+(5, 'Mass spec 2000', 1244, '2021-01-24', '1'),
+(6, 'Workbech ', 5656, '2021-02-03', '2'),
+(7, 'Workbech ', 4096, '2020-12-01', '3'),
+(8, 'Workbech ', 2687, '2021-02-01', '1'),
+(9, 'Workbech ', 3487, '2020-12-14', '1');
 
 
 ALTER TABLE instrument
   ADD PRIMARY KEY (Ins_ID);
 ALTER TABLE instrument ADD FULLTEXT KEY Room_ID (Room_ID);
+
+
+ALTER TABLE instrument
+  MODIFY Ins_ID int NOT NULL AUTO_INCREMENT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
