7c49f1d745587db9c09c9b8f367d5e95e227fc8b
unknown <oliviamnorlin@gmail.com>
2021-03-01 20:02:49 +0100

    some updates in all, wrote local storage stuff in login

--- server/login.js
+++ server/login.js
@@ -9,6 +9,8 @@ function check_login_data(req,res){
     utility.parse_data(req,(user_data)=>{
         console.log('>> user_data: ',user_data)   //user_data.password, user_data.email is a string 
         
+        //var sql="SELECT Email, Password, SSN, Admin, Maintenance FROM user WHERE Email = ?"
+        //Check maintenance spelling and name in user.sql when added
         database.connection.query("SELECT Email, Password, SSN FROM user WHERE Email = ?", [user_data.email], (error, result)=> {
             if(error){
                 console.log("error selecting attributes user",error)
@@ -25,9 +27,17 @@ function check_login_data(req,res){
                 console.log('>> db_data email: ', db_data[0].Email);
                 console.log(">> Email and password match found, login succesfull")
 
+                //Uncomment when Admin and Maintenance are added to local storage and in db
+                //string=fs.readFileSync("../html/overview.html",utility.encoding.utf8)
+                //string=string.replace("$$EMAIL$$", `"${db_data[0].Email}"`) //Add email to locale storage
+                //string=string.replace("$$SSN$$", `"${db_data[0].SSN}"`)     //Add SSN to local storage
+                //string=string.replace("$$ADMIN$$", `"${db_data[0].Admin}"`) //Add Admin to locale storage
+                //res.end(string.replace("$$MAINTENANCE$$", `"${db_data[0].Maintenance"`)) //Add Maintenance to local storage and send file. 
+
                 overview_string=fs.readFileSync("../html/overview.html",utility.encoding.utf8)
                 with_email=overview_string.replace("$$EMAIL$$", `"${db_data[0].Email}"`)
                 res.end(with_email.replace("$$SSN$$", `"${db_data[0].SSN}"`))
+        
                 console.log('>> overview.html file sent back to client')
             }
             else{
