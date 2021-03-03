25753e2c976eeba1c4c687d59da6f59b102ac0e8
unknown <oliviamnorlin@gmail.com>
2021-03-03 23:34:46 +0100

    wrote function

--- server/remove_user.js
+++ server/remove_user.js
@@ -3,17 +3,30 @@ const database=require("./database.js")
 
 /**
  * Removes user from the system by changing exist attribute to 0 for certain user
- * currently expects client data: SSN 
+ * currently expects client data: ssn
  * currently responds with: error/success
  * @param {Request} req Request object with client data
  * @param {Response} res Response object
  */
 
  function remove_user(req,res){
-    throw "unimplemented"
     utility.parse_data(req,(data)=>{
+      console.log(">>data: ", data) //for testing 
+      console.log(">>data: ", data.ssn) //for testing
 
+      var sql="UPDATE user SET Exist=0 WHERE SSN=?"
+      database.connection.query(sql, [data.ssn], (error, result)=> {
+         if(error){
+            const error_message="error when removing user:'"+error+"'"
+            utility.log(`${error_message}`,"error")
 
-    })
+            res.writeHeader(200,utility.content.json)
+            res.end(JSON.stringify({error:error_message}))
+            return
+        }
+        res.writeHeader(200,utility.content.json)
+        res.end(JSON.stringify({result:"successfully removed user"}))
+      })
+   })
  }
  module.exports.remove_user=remove_user
