8694ae6f1e7f7d30ebb5495fb17243c779f7d538
unknown <oliviamnorlin@gmail.com>
2021-03-04 19:40:54 +0100

    utility.log added

--- server/login.js
+++ server/login.js
@@ -13,26 +13,19 @@ const { Console } = require("console")
  **/
 
 function check_login_data(req,res){
-    //parse login data from req
-    console.log(">> check_login_data function")
+    
     utility.parse_data(req,(user_data)=>{
-        console.log('>> user_data: ',user_data)
         
         database.connection.query("SELECT Email, Password, SSN, Maintenance, Admin FROM user WHERE Email = ? and Exist=1", [user_data.email], (error, result)=> {
             if(error){
-                console.log("error selecting attributes user",error)
-                if(!error.fatal){
-                    database.disconnect(connection)
-                }
+                utility.log(`error selectiong attributes from user: ${error}`, 'error')
                 return
             }
             db_data=JSON.parse(JSON.stringify(result))  
-            console.log('>> db_data: ', db_data) 
             
             //Check email and password match 
             if(db_data.length!=0 && user_data.password.localeCompare(db_data[0].Password)==0){
-                console.log('>> db_data email: ', db_data[0].Email);
-                console.log(">> Email and password match found, login succesfull")
+                utility.log('Email and password match found, login succesfull')
 
                 string=fs.readFileSync("../html/overview.html",utility.encoding.utf8)
                 //Add email to locale storage
@@ -44,12 +37,12 @@ function check_login_data(req,res){
                 //Add Maintenance to local storage and send file to client
                 res.end(string.replace("$$MAINTENANCE$$", `"${db_data[0].Maintenance}"`))
 
-                console.log('>> overview.html file sent back to client')
+                utility.log('overview.html file sent back to client')
             }
             else{
-                console.log(">> No match found, login unsuccesfull")
+                utility.log("No match found, login unsuccesfull")
                 res.end(fs.readFileSync("../html/index.html",utility.encoding.utf8).replace("$$LOGINSUCCESS$$", "true"))
-                console.log('>> index.html file sent back to client with LOGINSUCCESS=true')
+                utility.log('index.html file sent back to client with LOGINSUCCESS=true')
             }
         })  
     })
