7647e8e3ba2def36afc1018452e2153c190a676c
unknown <oliviamnorlin@gmail.com>
2021-03-04 11:24:18 +0100

    fixed utility.log

--- server/get_maintenance.js
+++ server/get_maintenance.js
@@ -11,31 +11,27 @@ const database=require("./database.js")
 function get_maintenance(req,res) {
          
     utility.parse_data(req,(data)=>{
-        console.log('>>data: ', data) 
-        console.log('>> Ins_ID: ',data.Ins_ID)
-        
+    
         var sql ="SELECT DateTime, Status, Notes FROM ins_maintenance join instrument on ins_maintenance.Ins_ID=instrument.Ins_ID WHERE ins_maintenance.Ins_ID = ? and instrument.Exist =1"
             
         database.connection.query(sql, [data.Ins_ID], (error, result)=> {
             if(error){
                 const error_message="error selecting attributes: '"+error+"'"
-                console.log(error_message)
+                utility.log(`${error_message}`, 'error')
 
                 res.writeHeader(200,utility.content.json)
                 res.end(JSON.stringify({error:error_message}))
-
                 return
             }
-            db_maintenance=JSON.parse(JSON.stringify(result))  
-            console.log('>> db_maintenance: ', db_maintenance) 
+            db_maintenance=JSON.parse(JSON.stringify(result))   
 
             var ret=[];
             for(item of db_maintenance){
                 ret.push({DateTime:item.DateTime, Status:item.Status, Notes:item.Notes})
-            }   
+            } 
             res.writeHeader(200,utility.content.from_filename(".json"))
             res.end(JSON.stringify(ret))
-            console.log(">> Sent maintenance") 
+            utility.log(`Maintenance sent to client for insID ${data.Ins_ID}`) 
         })
     })
 }
