9d089e7d247206dfcd900e32fd31dabd1f65af0e
unknown <oliviamnorlin@gmail.com>
2021-03-04 12:04:05 +0100

    variable error changed

--- server/remove_instrument.js
+++ server/remove_instrument.js
@@ -3,7 +3,7 @@ const database=require("./database.js")
 
 /**
  * Removes instrument from the system by changing exist attribute to 0 for certain instrument
- * currently expects client data: ins_ID
+ * currently expects client data: ins_id
  * currently responds with: error/success
  * @param {Request} req Request object with client data
  * @param {Response} res Response object
@@ -12,10 +12,10 @@ const database=require("./database.js")
  function remove_instrument(req,res){
     utility.parse_data(req,(data)=>{
       console.log(">>data: ", data) //for testing 
-      console.log(">>data: ", data.ins_ID) //for testing
+      console.log(">>data: ", data.ins_id) //for testing
 
       var sql="UPDATE instrument SET Exist=0 WHERE Ins_ID=?"
-      database.connection.query(sql, [data.ins_ID], (error, result)=> {
+      database.connection.query(sql, [data.ins_id], (error, result)=> {
          if(error){
             const error_message="error when removing instrument:'"+error+"'"
             utility.log(`${error_message}`,"error")
