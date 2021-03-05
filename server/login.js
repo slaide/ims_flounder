cdc04f7a67470fd7510a64f66b3e669b72175383
slaide <patrick.hennig@fau.de>
2021-03-05 14:06:43 +0100

    changed login page and login.js code to use database function that is still unfinished

--- server/login.js
+++ server/login.js
@@ -13,37 +13,28 @@ const { Console } = require("console")
  **/
 
 function check_login_data(req,res){
-    
     utility.parse_data(req,(user_data)=>{
-        
-        database.connection.query("SELECT Email, Password, SSN, Maintenance, Admin FROM user WHERE Email = ? and Exist=1", [user_data.email], (error, result)=> {
-            if(error){
-                utility.log(`error selectiong attributes from user: ${error}`, 'error')
-                return
-            }
-            db_data=JSON.parse(JSON.stringify(result))  
-            
-            //Check email and password match 
-            if(db_data.length!=0 && user_data.password.localeCompare(db_data[0].Password)==0){
-                utility.log('Email and password match found, login succesfull')
+        database.account.login(user_data,error=>{
+            if(error.fatal) throw error;
 
-                string=fs.readFileSync("../html/overview.html",utility.encoding.utf8)
-                //Add email to locale storage
-                string=string.replace("$$EMAIL$$", `"${db_data[0].Email}"`) 
-                //Add SSN to local storage
-                string=string.replace("$$SSN$$", `"${db_data[0].SSN}"`)
-                //Add Admin to locale storage     
-                string=string.replace("$$ADMIN$$", `"${db_data[0].Admin}"`)
-                //Add Maintenance to local storage and send file to client
-                res.end(string.replace("$$MAINTENANCE$$", `"${db_data[0].Maintenance}"`))
+            utility.log(`error selecting attributes from user: ${error}`)
 
-                utility.log('overview.html file sent back to client')
+            res.writeHeader(200,utility.content.json)
+            res.end(`{error:${error}}`)
+
+            return
+        },results=>{
+            const user_data={
+                email:results[0].Email,
+                ssn:results[0].SSN,
+                admin:results[0].Admin,
+                maintenance:results[0].Maintenance,
+                token: utility.sha512(user_data.password,salt=utility.format_time(new Date))
             }
-            else{
-                utility.log("No match found, login unsuccesfull")
-                res.end(fs.readFileSync("../html/index.html",utility.encoding.utf8).replace("$$LOGINSUCCESS$$", "true"))
-                utility.log('index.html file sent back to client with LOGINSUCCESS=true')
-            }
+            res.writeHeader(200,utility.content.json)
+            res.end(JSON.stringify(user_data))
+
+            utility.log(`user ${user_data.ssn} successfully logged in`)
         })  
     })
 }
