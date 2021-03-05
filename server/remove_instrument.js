1bc07bb4028e1d1d68ad1f0af86ac12cec25f27d
unknown <oliviamnorlin@gmail.com>
2021-03-05 16:17:10 +0100

    swapped to database.instruments.remove

--- server/remove_instrument.js
+++ server/remove_instrument.js
@@ -9,22 +9,19 @@ const database=require("./database.js")
  * @param {Response} res Response object
  */
 
- function remove_instrument(req,res){
-    utility.parse_data(req,(data)=>{
+function remove_instrument(req,res){
+   utility.parse_data(req,(data)=>{
+      console.log("data: ", data)
+      database.instruments.remove(data,(error)=>{
+          if(error.fatal) throw error;
+          
+          res.writeHeader(200,utility.content.json)
+          res.end(JSON.stringify({error:error}))
 
-      var sql="UPDATE instrument SET Exist=0 WHERE Ins_ID=?"
-      database.connection.query(sql, [data.ins_id], (error, result)=> {
-         if(error){
-            const error_message="error when removing instrument:'"+error+"'"
-            utility.log(`${error_message}`,"error")
-
-            res.writeHeader(200,utility.content.json)
-            res.end(JSON.stringify({error:error_message}))
-            return
-        }
-        res.writeHeader(200,utility.content.json)
-        res.end(JSON.stringify({result:"successfully removed instrument"}))
-      }) 
-    })
- }
- module.exports.remove_instrument=remove_instrument
+      },(results)=>{
+          res.writeHeader(200,utility.content.json)
+          res.end(JSON.stringify({success:"successfully removed instrument"}))
+      })
+  })
+}
+module.exports.remove_instrument=remove_instrument
